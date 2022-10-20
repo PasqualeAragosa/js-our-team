05bbaf974844b45d0181fc67669f8492299bc20a
Pasquale Aragosa <aragosap@gmail.com>
2022-10-20 16:07:50 +0200

    add bonus folder

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,64 @@
+/*
+CONSEGNA
+
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1: Trasformare la stringa foto in una immagine effettiva
+BONUS 2: Organizzare i singoli membri in card/schede
+*/
+
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Waleter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    },
+];
+
+for (let i = 0; i < team.length; i++) {
+    console.log('Member Team ', i + 1);
+    console.log(team[i].name);
+    console.log(team[i].role);
+    console.log(team[i].image);
+}
+
+const containerEl = document.querySelector('.my_container');
+
+for (let i = 0; i < team.length; i++) {
+
+    const boxMarkup =  `<div class="box box_${i + 1}"></div>`;
+    const memberNameMarkup = `<p class="name">${team[i].name}</p>`;
+    const memberRoleMarkup = `<p class="role">${team[i].role}</p>`;
+    const memberImgMarkup = `<img src="./assets/img/${team[i].image}">`;
+    
+    containerEl.insertAdjacentHTML('beforeend', boxMarkup);
+    const divEl = document.querySelector(`.box_${i + 1}`);
+    divEl.insertAdjacentHTML('beforeend', memberImgMarkup);
+    divEl.insertAdjacentHTML('beforeend', memberNameMarkup);
+    divEl.insertAdjacentHTML('beforeend', memberRoleMarkup);
+}
